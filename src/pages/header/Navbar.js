dd070190a764bcbcee2409d7d8d987ab4d6ee829
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-28 12:09:36 +0600

    logout system applied

--- src/pages/header/Navbar.js
+++ src/pages/header/Navbar.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import ButtonMe from '../shared/ButtonMe';
+import auth from "../../firbase.init";
+import ButtonMe from "../shared/ButtonMe";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
 
 const Navbar = () => {
-    const navigate = useNavigate();
-    const btnclik = () => {
-        navigate('/login')
+  const navigate = useNavigate();
+  const [user, loading, error] = useAuthState(auth);
+  const signoutBtn = () => {
+    const procced = window.confirm("log out?");
+    if (procced) {
+      signOut(auth).then(() => {
+      });
     }
-    return (
-        <nav className='flex gap-5 items-center'>
-            <Link to='/'>Home</Link>
-            <ButtonMe btn={btnclik}>Login</ButtonMe>
-        </nav>
-    );
+  };
+
+  return (
+    <nav className="flex gap-5 items-center">
+      <Link to="/">Home</Link>
+      {user ? (
+        <ButtonMe btn={signoutBtn}>Logout</ButtonMe>
+      ) : (
+        <ButtonMe btn={() => navigate("/login")}>Login</ButtonMe>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
