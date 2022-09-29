60ecc86d7fd2f0133cf1f47094d163b7265d7948
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-29 14:32:10 +0600

    header sticky  now

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -3,8 +3,8 @@ import Navbar from './Navbar';
 
 const Header = () => {
     return (
-        <div className='m-3 mx-16 flex justify-between'>
-            <img src="" alt="logo" />
+        <div className='p-3 bg-white sticky top-0 px-16 flex justify-between'>
+            <h1 className='text-3xl cursor-pointer text-center'>Kabirs Inventory</h1>
             <Navbar></Navbar>
         </div>
     );
