67920c333842c37f5c64eadf850d59efab08954b
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-29 13:23:34 +0600

    default sysytem aapplied for user base

--- src/pages/Home/InstockProducts/InstockProducts.js
+++ src/pages/Home/InstockProducts/InstockProducts.js
@@ -6,40 +6,67 @@ import useLoadStocks from "../../../hooks/useLoadStocks";
 import ButtonMe from "../../shared/ButtonMe";
 
 const InstockProducts = () => {
-  const  [user] = useAuthState(auth);
-  
-  const [userProducts, setUserProducts] = useLoadStocks('https://safe-garden-23742.herokuapp.com/userData?email='+user?.email);
-  
-  console.log(userProducts);
-  
+  const [user] = useAuthState(auth);
+
+  const [userProducts, setUserProducts] = useLoadStocks(
+    "https://safe-garden-23742.herokuapp.com/userData?email=" + user?.email
+  );
+
+  console.log("user products", userProducts);
+
   const [products, setProducts] = useLoadStocks(
     "https://safe-garden-23742.herokuapp.com/inStocProducts"
   );
+  console.log("products", products);
   const navigate = useNavigate();
 
   return (
     <div className="m-12">
       <h3 className="text-3xl text-center mb-12">In stock products</h3>
       <div className="grid grid-cols-3 gap-4">
-        {products.map((product) => (
-          <div key={product?._id} className="border">
-            <img
-              className="w-full"
-              src="https://betterstudio.com/wp-content/uploads/2019/05/1-1-instagram-1024x1024.jpg"
-              alt=""
-            />
-            <div className="p-2">
-              <h4 className="text-xl font-bold">{product?.name}</h4>
-              <p className="font-mono">price: {product?.balance}</p>
-              <p>availble: {product?.age} pis</p>
-              <p>{product?.about.slice(0, 90)}</p>
-              <p className="font-bold mb-5">{product?.company}</p>
-              <ButtonMe btn={() => navigate("/manageproducts/" + product?._id)}>
-                Update this product
-              </ButtonMe>
-            </div>
-          </div>
-        ))}
+        {user
+          ? userProducts.map((product) => (
+              <div key={product?._id} className="border">
+                <img
+                  className="w-full"
+                  src="https://betterstudio.com/wp-content/uploads/2019/05/1-1-instagram-1024x1024.jpg"
+                  alt=""
+                />
+                <div className="p-2">
+                  <h4 className="text-xl font-bold">{product?.name}</h4>
+                  <p className="font-mono">price: {product?.balance}</p>
+                  <p>availble: {product?.age} pis</p>
+                  <p>{product?.about.slice(0, 90)}</p>
+                  <p className="font-bold mb-5">{product?.company}</p>
+                  <ButtonMe
+                    btn={() => navigate("/manageproducts/" + product?._id)}
+                  >
+                    Update this product
+                  </ButtonMe>
+                </div>
+              </div>
+            ))
+          : products.map((product) => (
+              <div key={product?._id} className="border">
+                <img
+                  className="w-full"
+                  src="https://betterstudio.com/wp-content/uploads/2019/05/1-1-instagram-1024x1024.jpg"
+                  alt=""
+                />
+                <div className="p-2">
+                  <h4 className="text-xl font-bold">{product?.name}</h4>
+                  <p className="font-mono">price: {product?.balance}</p>
+                  <p>availble: {product?.age} pis</p>
+                  <p>{product?.about.slice(0, 90)}</p>
+                  <p className="font-bold mb-5">{product?.company}</p>
+                  <ButtonMe
+                    btn={() => navigate("/manageproducts/" + product?._id)}
+                  >
+                    Update this product
+                  </ButtonMe>
+                </div>
+              </div>
+            ))}
       </div>
       <div className="text-center mt-12">
         <ButtonMe btn={() => navigate("/managestock")}>
