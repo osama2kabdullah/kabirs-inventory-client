18f97734f256c6cddf8c3fdf92b3621d12f8f83d
osama-abdullah <osama2kabdullah@gmail.com>
2022-09-29 14:41:17 +0600

    bannar size decrese and header logo click to redirect home page

--- src/pages/Home/Bannar.js
+++ src/pages/Home/Bannar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Bannar = () => {
     return (
-        <div className='h-screen bg-red-300 flex justify-center items-center'>
+        <div className='h-[70vh] bg-red-300 flex justify-center items-center'>
             <h1 className='text-5xl'>Your warehouse products management helper</h1>
         </div>
     );
--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 
 const Header = () => {
+    const navigate = useNavigate();
     return (
         <div className='p-3 bg-white sticky top-0 px-16 flex justify-between'>
-            <h1 className='text-3xl cursor-pointer text-center'>Kabirs Inventory</h1>
+            <h1 onClick={()=>navigate('/')} className='text-3xl cursor-pointer text-center'>Kabirs Inventory</h1>
             <Navbar></Navbar>
         </div>
     );
